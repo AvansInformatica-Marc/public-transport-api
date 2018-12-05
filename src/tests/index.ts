3fc34c1b726dd8bc1a14a358604462795de9ce85
Marc <marc.j.bouwman@gmail.com>
2018-12-05 17:55:59 +0100

    Add tests

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,1 +1,11 @@
-// TODO
+import * as chai from "chai"
+import "mocha";
+import Operator from "../main/models/Operator";
+
+chai.should()
+
+describe(`Unit tests`, () => {
+    it(`Test 1`, () => {
+        new Operator("Test").should.have.property('name').that.is.a('string').which.equals('Test')
+    })
+})
