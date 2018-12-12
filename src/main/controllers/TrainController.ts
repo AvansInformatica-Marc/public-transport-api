00d4ca4db51b4656fd1e749ab0ca904b6b222aa2
Marc <marc.j.bouwman@gmail.com>
2018-12-12 20:02:28 +0100

    Add admin to auth

--- src/main/controllers/TrainController.ts
+++ src/main/controllers/TrainController.ts
@@ -7,43 +7,43 @@ import { Entity } from "../models/db/Entity";
 
 type json = {[key: string]: any}
 
-export default class TrainController implements Controller<Train, Entity<Operator>> {
+export default class TrainController implements Controller<Entity<Train>, Entity<Operator> | { admin: true }> {
     public resourceName: string = "trains"
 
     constructor(protected repo: Repository<Train>){}
     
-    public async get(id: string, _params: json, _auth?: Entity<Operator>): Promise<Train> {
+    public async get(id: string, _params: json, _auth?: Entity<Operator> | { admin: true }): Promise<Entity<Train>> {
         const model = await this.repo.getById(id)
         if(!model) throw new HttpErrors.Client.NotFound()
         return model
     }
 
-    public async getAll(_params: json, _auth?: Entity<Operator>): Promise<Train[]> {
+    public async getAll(_params: json, _auth?: Entity<Operator> | { admin: true }): Promise<Entity<Train>[]> {
         return await this.repo.getAll()
     }
 
-    public async create(model: json, _params: json, auth?: Entity<Operator>): Promise<Train> {
+    public async create(model: json, _params: json, auth?: Entity<Operator> | { admin: true }): Promise<Entity<Train>> {
         if(!auth) throw new HttpErrors.Client.Unauthorised()
         const train = this.validateModel(model)
         return await this.repo.create(train)
     }
 
-    public async update(id: string, model: json, _params: json, auth?: Entity<Operator>): Promise<void> {
+    public async update(id: string, model: json, _params: json, auth?: Entity<Operator> | { admin: true }): Promise<void> {
         if(!auth) throw new HttpErrors.Client.Unauthorised()
         const train = this.validateModel(model)
         await this.repo.update(id, train)
     }
 
-    public updateAll(_model: json, _params: json, _auth?: Entity<Operator>): void | Promise<void> {
+    public updateAll(_model: json, _params: json, _auth?: Entity<Operator> | { admin: true }): void | Promise<void> {
         throw new HttpErrors.Client.MethodNotAllowed()
     }
 
-    public async delete(id: string, _params: json, auth?: Entity<Operator>): Promise<void> {
+    public async delete(id: string, _params: json, auth?: Entity<Operator> | { admin: true }): Promise<void> {
         if(!auth) throw new HttpErrors.Client.Unauthorised()
         await this.repo.delete(id)
     }
 
-    public deleteAll(_params: json, _auth?: Entity<Operator>): void | Promise<void> {
+    public deleteAll(_params: json, _auth?: Entity<Operator> | { admin: true }): void | Promise<void> {
         throw new HttpErrors.Client.MethodNotAllowed()
     }
 
